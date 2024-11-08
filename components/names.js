31d09b947b0a3d66a5a3a17bd4a0cd4d397a369b
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-08 00:59:38 -0300

    03-State and Input
    03-State and Input

--- components/names.js
+++ components/names.js
@@ -1,9 +1,43 @@
-import { Text} from 'react-native'
+import { useState } from 'react'
+import { Alert, Button, StyleSheet, Text, TextInput, View} from 'react-native'
+import { AppStyles } from '../constants/styles'
+
+const DEFAULTS = {
+    modal:false,
+    inputName:'',
+    names:[],
+    result:''
+}
 
 export default function Names(){
+    const [state, setState ] = useState(DEFAULTS)
+    const handleInput = (value)=> setState({...state,inputName:value})
+
     return(
-        <Text>
-            Names
-        </Text>
+        <View style={{width:'80%'}}>
+            <>
+                <TextInput
+                    value={state.inputName}
+                    onChangeText={(value)=>handleInput(value)}
+                    style={styles.input}
+                />
+                <Button
+                    title='Add name'
+                    onPress={()=> Alert.alert('Hello')}
+                />
+            </>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input:{
+        borderRadius:8,
+        fontSize:20,
+        marginBottom:20,
+        borderColor: AppStyles.color.airBlue,
+        borderWidth:1,
+        padding:10,
+        backgroundColor:'#fff'
+    }
+})
