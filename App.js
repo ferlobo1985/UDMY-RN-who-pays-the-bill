927ddcf9dbb23fc60ddd3e8941f182cfc995f25e
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-08 00:45:59 -0300

    02-Background and Icon
    02-Background and Icon

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import Pattern from './assets/pattern/ptrn-1.png';
+import { AppStyles } from './constants/styles';
+import Names from './components/names';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={Pattern}
+      resizeMode='cover'
+      style={styles.container}
+    >
+      <View style={{width:'100%', alignItems:'center'}}>
+        <Ionicons name="cash-outline" size={90} color={AppStyles.color.airBlue}/>
+        <Names/>
+        <StatusBar style='dark'/>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:80
   },
 });
---
+++ constants/styles.js
@@ -0,0 +1,9 @@
+
+
+export const AppStyles = {
+    color:{
+        darkBlue:'#283044',
+        airBlue:'#78A1BB',
+        cream:'#EBF5EE'
+    }
+}
